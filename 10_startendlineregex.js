22bca98fc3724468aef9c41a14c5704f448668aa
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-02-25 00:22:18 -0800

    start and end characters added in 10_startend.js

---
+++ 10_startendlineregex.js
@@ -0,0 +1,29 @@
+//Example 9
+// TIP: ALWAYS WRITE AS SPECIFIC REGULAR EXPRESSIONS AS POSSIBLE
+// Why? avoid real world scenarios.
+// ^, $ specify the start and end of the line, respectively
+
+function teststring(str) {
+//solutions
+//no other solution
+
+  let regex = /^Mission: successful$/g;
+  let found = str.match(regex);
+
+  if (found !== null) {
+    return true;
+  }
+
+}
+
+//skip the last element
+let str1 = ['Mission: successful', 'Last Mission: unsuccessful', 'Next Mission: successful upon capture of target'];
+let newstr = [];
+
+for (let word of str1) {
+  if(teststring(word)){
+    newstr.push(word);
+  }
+}
+console.log(str1.join(' '));
+console.log(newstr.join(' '));
