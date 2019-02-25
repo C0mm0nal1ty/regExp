404dd12e1ff918545229567956c352dcc52e0e7d
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-02-24 21:56:18 -0800

    adding excercise on optionality.

---
+++ 8_optionality.js
@@ -0,0 +1,25 @@
+//Example 8
+// ? - optionality, metacharacter
+// allows you to match a either 0 or 1 of the preceding characters
+// ab?c matches abc or ac
+
+function teststring(str) {
+  let regex = /[a]+[bc]+/g;
+  let found = str.match(regex);
+
+  if (found !== null) {
+    return true;
+  }
+
+}
+
+let str1 = ['1 files found?', '2 files found?', '2 files found?','No files found?.'];
+let newstr = [];
+
+for (let word of str1) {
+  if(teststring(word)){
+    newstr.push(word);
+  }
+}
+console.log(str1.join(' '));
+console.log(newstr.join(' '));
